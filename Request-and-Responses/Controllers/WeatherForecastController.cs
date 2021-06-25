dc59ec5e4d5753bb860717dd74e1774ea0aba59b
Komro <34216@nelnet.net>
2021-06-25 13:25:14 -0500

    Request-and-Responses exercise final push

---
+++ Request-and-Responses/Controllers/WeatherForecastController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Request_and_Responses.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("getHeader")]
+        public IActionResult GetHeader()
+        {
+            var headers = base.Request.Headers;
+            Console.WriteLine(headers);
+            return base.Ok(headers);
+        }
+
+        [HttpGet("getQuery")]
+        public IActionResult GetQuery()
+        {
+            var query = base.Request.QueryString;
+            Console.WriteLine(query);
+            return base.Ok(query);
+        }
+    }
+}
